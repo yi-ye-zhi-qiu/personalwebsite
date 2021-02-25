de4e606d43e57e8ec6245b91d0676c44525ee667
Liam Isaacs <yeqiu194@gmail.com>
2021-02-25 12:47:11 -0800

    add readable notes on code

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+#Import flask
 from flask import *
 from flask import Flask, render_template, request, jsonify, make_response
+
+#import models
 from models.liamometer import get_movie_data
-from riotwatcher import LolWatcher, ApiError
 from models.liam_gg import game_info_by_match_id
-#from models.liam_gg_ml import give_shap_plot
+
+#import this for lieum.gg/liam.gg to work
+from riotwatcher import LolWatcher, ApiError
 import pandas as pd
+
+#from models.liam_gg_ml import give_shap_plot
+
 # import shap
 # shap.initjs()
 # import matplotlib
@@ -12,79 +19,75 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/liamometer')
-def show_mango():
-    """
-    Performs linear regression on scraped databases (rottentomatoes, metacritic, IMDB, BoxOfficeMojo),
-    takes in these 5 CSVs.
-    Returns 1500 rows, just because I do not want to overload the page (currently working on lazy-load in javascript, or loading as you scroll.)
-    """
-    max, liams_favorite, liams_favorite_movie_image, html_data = get_movie_data()
-
-
-    return render_template('liamometer.html', max=max, liams_favorite_movie_image = liams_favorite_movie_image,
-                            liams_favorite=liams_favorite, html_data = html_data)
-
+#Home/landing page
 @app.route('/')
 def show_index():
+    #just serve up index.html
     return render_template('index.html')
 
-@app.route('/portfolio')
-def show_portfolio():
-    return render_template('portfolio.html')
-
+#Petrarch NLP project page
 @app.route('/petrarch')
 def show_petrarch():
     return render_template('petrarch.html')
 
+#Lieum.gg SEARCH page
 @app.route('/league')
 def show_league():
     return render_template('league.html')
 
-#on page load of liamgg.html
+#Lieum.gg POST-SEARCH page
 @app.route('/league', methods=["GET", "POST"])
 def riot_api_call():
+    #Check if post method, if so store username searched as "name" variable
     if request.method == 'POST':
         form = request.form
     for key in form:
         name = form[key]
 
-    api_key = ''
-    gamemode = 'CLASSIC'
-    region = 'na1'
+    #Define requisite variables for riot api call
+    api_key = '' #get one @ https://developer.riotgames.com/
+    region = 'na1' #this is only functional right now for North American data (LoL has European, Chinese, etc. servers)
     watcher = LolWatcher(api_key)
+    #Define the user using python wrapper RiotWatcher for League of Legends data
     user = watcher.summoner.by_name('na1', name)
+    #Get matchlist of the user
     matches = watcher.match.matchlist_by_account(region, user['accountId'])
 
+    #get most recent 10 games of that user and the relevant GameIDs
+    #change to 20, 30 if you want to display more data on the page
+    #You may run into an API limit error doing that, though
     game_ids = []
-
-    for i in range(0,10): #display 10 games
+    for i in range(0,10):
         game_ids.append(matches['matches'][i]['gameId'])
 
+    #Get the user's ranked stats
     ranked_info = game_info_by_match_id(api_key, name, region,
-                                        gamemode, game_ids[0]).rank_stats()
+                                        game_ids[0]).rank_stats()
     ranked_info = pd.DataFrame(ranked_info)
+    #If unranked just put "unranked"
     if ranked_info.empty:
         ranked_info = pd.DataFrame({'tier': ['unranked']})
-
+    #Get lowercase value of stuff like DIAMONDI, DIAMONDII etc.
     rank_league = ranked_info['tier'].values[0].lower()
-
-
+    #profile picture is going to be hamstercam for unranked or the ranked picture otherwise
     if rank_league == 'unranked':
         rank = "static/images/ranked-emblems/hamster_cam.jpeg"
     else:
         rank = "static/images/ranked-emblems/"+rank_league+".png"
 
+    #Assemble dataframes for each GameID (1 api request per game)
     dfs = {}
     for gameid in game_ids:
         dfs[gameid] = game_info_by_match_id(api_key,
                                           name, region,
                                           gamemode, gameid).match_data()
 
+    #Assemble SHAP plots for each GameID (this is revealed by the show stats button)
     shap_plots = {}
     for b in dfs:
          shap_plots[b] = 'shap plot goes here'#give_shap_plot(dfs[b], name)
 
+    #serve up liam.gg.html, but pass all of our variables as needed by Django
     return render_template('public/liam.gg.html', rank=rank,
                                                   ranked_info=ranked_info,
                                                   game_ids = game_ids,
@@ -92,7 +95,6 @@ def riot_api_call():
                                                   dfs=dfs,
                                                   shap_plots = shap_plots,
                                                   name=name)
-
 
 if __name__ == '__main__':
     app.run()
--- models/liam_gg.py
+++ models/liam_gg.py
@@ -1,18 +1,19 @@
-from riotwatcher import LolWatcher, ApiError
+#Import libraries
+from riotwatcher import LolWatcher, ApiError #this is the python wrapper we use,
+#                                             documentation at https://riot-watcher.readthedocs.io/en/latest/riotwatcher/LeagueOfLegends/index.html
 import pandas as pd
 import numpy as np
 import pprint
 from datetime import datetime
 
 
-# Fetching game data
-
 class game_info_by_match_id():
     """
     Returns df of user info from a given match (so far).
+    We pass in the user's name, region, game id of that game, and the api_key.
+    We get a datframe that has a lot of information about the match -- every champion, every item, every ward, etc.
     """
-    #define private variables to use in class
-
+    #Constructor function
     def __init__(self, api_key, name, region, gamemode, gameid):
         #upon calling the class we pass in a bunch of things to initialize^
         self.api_key = api_key
@@ -23,27 +24,33 @@ class game_info_by_match_id():
         watcher = LolWatcher(self.api_key)
         self.user = watcher.summoner.by_name(region, name)
 
+    #This is used to return DIAMONDI, DIAMONDII, etc. of the player.
     def rank_stats(self):
         watcher = LolWatcher(self.api_key)
-
         #league, division, games played, etc.
         encrypted_summoner_id = self.user['id']
         self.rank_stats = watcher.league.by_summoner(self.region, self.user['id'])
         return self.rank_stats
 
+    #THIS is our bread and gravy -- this returns a dataframe.
     def match_data(self):
+        """
+        Returns dataframe per GameID.
+        """
         watcher = LolWatcher(self.api_key)
 
+        # ========= GET RECENT MATCHES OF USER =========== #
         self.matches = watcher.match.matchlist_by_account(self.region, self.user['accountId'])
-
         self.match_data = watcher.match.by_id(self.region, self.gameid)
         m = self.match_data
 
+        # ========= INFORMATION FOR EACH PLAYER IN A GIVEN MATCH, STORED IN DICTIONARY ======= #
+
         #n is for each "participant" or player in the match
-        def gd():
+        def gd(): #gd = get data
             n = [] #dump raw stats into here from participants
             for row in m['participants']:
-                m_row = {}
+                m_row = {} #store in dict
                 m_row['champion'] = row['championId']
                 m_row['spell1'] = row['spell1Id']
                 m_row['spell2'] = row['spell2Id']
@@ -109,6 +116,10 @@ class game_info_by_match_id():
                 m_row['magicDamageDealt'] = row['stats']['magicDamageDealt']
                 m_row['goldSpent'] = row['stats']['goldSpent']
                 m_row['neutralMinionsKilled'] = row['stats']['neutralMinionsKilled']
+                m_row['totalTimeCrowdControlDealt'] = row['stats']['totalTimeCrowdControlDealt']
+                #Use try/except loops for fields that may be blank if the user played an ARAM game,
+                #for those unfamiliar w/ League, there are no "Jungles" in ARAM, so this stat will be non-existent.
+                #We must account for this, or else we get an error.
                 try:
                     m_row['neutralMinionsKilledTeamJungle'] = row['stats']['neutralMinionsKilledTeamJungle']
                 except:
@@ -117,7 +128,6 @@ class game_info_by_match_id():
                     m_row['neutralMinionsKilledEnemyJungle'] = row['stats']['neutralMinionsKilledEnemyJungle']
                 except:
                     m_row['neutralMinionsKilledEnemyJungle'] = ''
-                m_row['totalTimeCrowdControlDealt'] = row['stats']['totalTimeCrowdControlDealt']
                 try:
                     m_row['wardsPlaced'] = row['stats']['wardsPlaced']
                 except:
@@ -129,10 +139,15 @@ class game_info_by_match_id():
                 n.append(m_row)
             return n
 
-        n = gd()
-        for i in range(0,len(n)):
+        n = gd() #n is an array with a dict per player (10 plays per match), that has a bunch of information
+
+        for i in range(0,len(n)): #for each one, we will add in information that's NOT in 'participants'
             n[i]['summonerName'] = m['participantIdentities'][i]['player']['summonerName']
             n[i]['profileIcon'] = m['participantIdentities'][i]['player']['profileIcon']
+
+        # ========= CONVERT INTEGER VALUES FOR CHAMPIONS, ITEMS, AND SUMMONER SPELLS
+        #           INTO STRINGS, for instance champion '200' is now going to be
+        #           'Championname', like 'Ashe' or something                         ======== #
 
         latest = watcher.data_dragon.versions_for_region(self.region)['n']['champion']
         static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
@@ -148,6 +163,7 @@ class game_info_by_match_id():
             for key in static_summonerspell_list['data']:
                 row = static_summonerspell_list['data'][key]
                 summonerspell_dict[row['key']] = row['id']
+                #This is the image for each summoner spell
                 summonerspell_dict[row['key']] = spell_url + str(row['image']['full'])
 
             #champs
@@ -158,7 +174,7 @@ class game_info_by_match_id():
             for key in static_champ_list['data']:
                 row = static_champ_list['data'][key]
                 champ_name_dict[row['key']] = row['id']
-               # champ_dict[row['image']] = champ_url + str(row['image']['full'])
+                #This is the image for each champion
                 champ_image_dict[row['key']] = champ_url +  str(row['image']['full'])
 
             #items
@@ -176,17 +192,24 @@ class game_info_by_match_id():
                 row['championImage'] = champ_image_dict[str(row['champion'])]
                 for i in range(0,7):
                     try:
+                        #get names and images
                         row['itemName' +str(i)] = item_dict[str(row['item'+str(i)])]
                         row['itemImage' + str(i)] = item_url + str(row['item'+str(i)]) + '.png'
                     except:
+                        #what if the item is blank since the user did not buy anything? then the name is set to 0
                         row['itemName' +str(i)] = 0
                 row['spell1Image'] = summonerspell_dict[str(row['spell1'])]
                 row['spell2Image'] = summonerspell_dict[str(row['spell2'])]
 
                 row['profileIconImage'] = 'http://ddragon.leagueoflegends.com/cdn/11.2.1/img/profileicon/' + str(row['profileIcon']) + '.png'
 
+
+            #STORE IN DATAFRAME
             df = pd.DataFrame(n)
 
+            # ======== MVP, DOUBLEKILL STUFF TO DISPLAY ON HTML ========= #
+
+            #We only have MVP which we unanimously award to my friend Timmy and no one else
             if self.name == 'Divine Right':
                 df['MVP'] = 'MVP'
             else:
@@ -195,6 +218,9 @@ class game_info_by_match_id():
             return df
 
         df = g_c(n)
+
+        # =========== ADDING IN DF COLUMNS WHICH WE NEED TO CALCULATE ======== #
+
         #add in extra columns
         df['gameDur'] = m['gameDuration']
         df['gameDuration'] = round((m['gameDuration'] / 60),2)
@@ -209,14 +235,13 @@ class game_info_by_match_id():
         df['playerDamageAsFractionOfTeamDamage'] = df['totalDamageDealtToChampions'] / df['teamTotalDamage']
         df['playerDamageAsFractionOfTeamDamage'] = round(df['playerDamageAsFractionOfTeamDamage'],2)*100
 
-        df.loc[(df.kda == np.inf), 'kda'] = 'BEAST-MODE'
+        #If KDA is 0, set it to say "expert"
+        df.loc[(df.kda == np.inf), 'kda'] = 'expert'
 
         #get time since last played (in days)
-
         now = datetime.now()
         last_game_played_when = df['gameCreation'].values[0]
         last_game_played_when = datetime.fromtimestamp(last_game_played_when)
-
         df['lastGamePlayedWhen'] = (now - last_game_played_when).days
 
         #get first blood, baron kills, etc.
@@ -231,6 +256,7 @@ class game_info_by_match_id():
                 df[i] = x(i)
             return df
 
+        #display game duration not as '111203123091203912309' but as '10 min 2 s' for instance
         def game_dur(x):
             x = str(x).split('.')
             return x[0] + ' m ' + x[1] + ' s'
