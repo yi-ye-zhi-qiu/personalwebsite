c2867bcad4d6e2e310e5ac4e1452ec3e5d663203
Liam Isaacs <yeqiu194@gmail.com>
2021-02-28 16:09:09 -0800

    add embedding projector JSON file to host

---
+++ templates/petrarch_tensors.js
@@ -0,0 +1,13 @@
+{
+  "embeddings": [
+    {
+      "tensorName": "My tensor",
+      "tensorShape": [
+        1000,
+        50
+      ],
+      "tensorPath": "https://gist.github.com/yi-ye-zhi-qiu/5b7f107e5b926a6b02bb900753aaa4a7",
+      "metadataPath": "https://gist.github.com/yi-ye-zhi-qiu/69a200a737690e6032513541fa1311e8"
+    }
+  ]
+}
